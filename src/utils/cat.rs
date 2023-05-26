2f6e74de20c2b115c1b30d99dc77ffb72c17f7f8
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-05-25 20:21:31 -0700

    Update cat to use new bufinput stuff

--- src/utils/cat.rs
+++ src/utils/cat.rs
@@ -4,13 +4,14 @@
  */
 
 use std::fs::File;
-use std::io::{copy, prelude::*, stdin, stdout, BufRead, BufReader};
+use std::io::{self, Write};
 
 use getargs::{Opt, Options};
 
-use crate::err::{AppletError, AppletResult};
+use crate::bufinput::BufInput;
+use crate::err::{Error, Result};
 
-pub fn util_cat(args: Vec<String>) -> AppletResult {
+pub fn util_cat(args: Vec<String>) -> Result {
     let mut opts = Options::new(args.iter().skip(1).map(String::as_str));
     while let Some(opt) = opts.next_opt().expect("argument parsing error") {
         match opt {
@@ -23,39 +24,43 @@ pub fn util_cat(args: Vec<String>) -> AppletResult {
         }
     }
 
-    let mut files: Vec<(&str, Box<dyn BufRead>)> = Vec::new();
+    let mut files: Vec<(&str, BufInput)> = Vec::new();
 
     for filename in opts.positionals() {
-        let file = File::open(filename);
-        match file {
-            Ok(file) => {
-                files.push((filename, Box::new(BufReader::new(file))));
-            }
-            Err(e) => {
-                return Err(AppletError::new(
-                    1,
-                    format!("Could not open file: {}: {}", filename, e),
-                ));
+        if filename == "-" {
+            files.push(("stdin", BufInput::Standard(io::stdin().lock())));
+        } else {
+            let file = File::open(filename);
+            match file {
+                Ok(file) => {
+                    files.push((filename, BufInput::File(io::BufReader::new(file))));
+                }
+                Err(e) => {
+                    return Err(Error::new(
+                        1,
+                        format!("Could not open file: {}: {}", filename, e),
+                    ));
+                }
             }
         }
     }
 
     if files.is_empty() {
         // If ain't nobody got me, stdin got me.
-        files.push(("stdin", Box::new(BufReader::new(stdin()))));
+        files.push(("stdin", BufInput::Standard(io::stdin().lock())));
     }
 
     for (filename, mut file) in files {
-        if let Err(e) = copy(&mut file, &mut stdout()) {
-            return Err(AppletError::new(
+        if let Err(e) = io::copy(&mut file, &mut io::stdout()) {
+            return Err(Error::new(
                 1,
                 format!("Could not write to {}: {}", filename, e),
             ));
         }
     }
 
-    if let Err(e) = stdout().flush() {
-        return Err(AppletError::new(
+    if let Err(e) = io::stdout().flush() {
+        return Err(Error::new(
             1,
             format!("Could not write to stdout: {}", e),
         ));
