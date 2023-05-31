834d1f559d8c676a99cc747d414266c7f80f751f
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-05-30 19:38:25 -0700

    Remove some redundancy in the argument parsing of sleep

--- src/utils/sleep.rs
+++ src/utils/sleep.rs
@@ -36,13 +36,7 @@ pub fn util(args: &[String]) -> Result {
     // We also don't have a better method to convert, unless we use nightly, so meh.
     #[allow(clippy::cast_sign_loss)]
     #[allow(clippy::cast_possible_truncation)]
-    let sleep_nsec = (args
-        .get(1)
-        .ok_or_else(|| usage(&args[0]))?
-        .parse::<f32>()
-        .map_err(|_| usage(&args[0]))?
-        * 1e9)
-        .round() as u64;
+    let sleep_nsec = (arg.parse::<f32>().map_err(|_| usage(&args[0]))? * 1e9).round() as u64;
 
     sleep(Duration::from_nanos(sleep_nsec));
 
