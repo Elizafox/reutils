c078f5f46313bab159e2cacf1c6a374f59cdc078
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2023-06-04 13:58:51 -0700

    Fix header

--- src/err.rs
+++ src/err.rs
@@ -1,4 +1,4 @@
-/* error.rs - error routines for reutils
+/* err.rs - error routines for reutils
  * Copyright (C) 2023 Elizabeth Myers. All rights reserved.
  * SPDX-License-Identifier: GPL-2.0-only
  */
